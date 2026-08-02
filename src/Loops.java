0d90e6b2684c67658a1caf7d913679cee1c2a450
Kushagra <kushagrab453@gmail.com>
2026-08-02 11:29:21 +0530

    Inittial commit

---
+++ src/Loops.java
@@ -0,0 +1,51 @@
+public class Loops {
+    public static void main(String [] args) {
+//        for(int i = 1; i <= 5; i++) {
+//            for(int j = 1; j <= 5; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i = 1; i <= 10; i++) {
+//            if(i == 2 || i == 4 || i == 6)
+//                continue;
+//            System.out.println(i);
+//        }
+
+// WhileLoop
+//        // initialize
+//        int i = 1;
+//        // condition
+//        while(i <=5) {
+//            // process
+//            System.out.println("Kushagra Bhatt");
+//            // updation
+//            i++;
+//        }
+
+//// nested while
+//        int i = 1;
+//        while(i <=2) {
+//            int j =1;
+//
+//            while(j <= 4){
+//                System.out.println("i =" + i + ", j =" + j);
+//                j++;
+//            }
+//            i++;
+//        }
+
+
+// Do while
+
+//        int i = 1;
+//        do {
+//            System.out.println("Kushagra Bhatt");
+//            i++;
+//        } while (i<=10);
+//
+
+    }
+}
+
+
