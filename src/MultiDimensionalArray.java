a30a7b995c1cce6fd0b01e84ef11f49ee6a604f2
Kushagra Bhatt <kushagrab453@gmail.com>
2026-08-06 21:58:00 +0530

    Add files via upload

---
+++ src/MultiDimensionalArray.java
@@ -0,0 +1,117 @@
+import java.util.*;
+public class MultiDimensionalArray {
+    public static void main(String [] args) {
+//        declaration
+        int[][] arr;
+//        allocation
+        arr = new int [3][4];
+
+//        initialzation
+//        int [][] brr = {
+//                {1,2},
+//                {2,4},
+//                {3,6},
+//                {4,8}
+//
+//        };
+////        System.out.println(brr[2][1]);
+//        int rowLength = brr.length;
+//        // only works when the column length is same
+//        int colLength = brr[0].length;
+//
+//        for (int i = 0; i <=rowLength-1; i++) {
+//            for (int j =0; j<= colLength-1; j++) {
+//                System.out.print(brr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+
+//       for Jaggeg Array
+//        int [][] brr = {
+//                {1,2},
+//                {2,4,5,6,9},
+//                {3,6,5,6},
+//                {4,8}
+
+//        };
+//        int rowLength = brr.length;
+//        // only works when the column lenght is same
+//        for (int rowIndex = 0; rowIndex <=rowLength-1; rowIndex++) {
+//            // Jaise hi main kisi nayi row m aaya, same point per mene us row ka colLength find kar liya
+//            // current row -> brr[rowIndex]
+//            // isme kitne columns -> brr[rowIndex].length
+//            int colLength = brr[rowIndex].length;
+//            for (int colIndex =0; colIndex<= colLength-1; colIndex++) {
+//                System.out.print(brr[rowIndex][colIndex] + " ");
+//            }
+//            System.out.println();
+//        }
+//
+        // more optimized solution for any 2-D array travarsal.
+//        for (int rowIndex = 0; rowIndex<= brr.length-1; rowIndex++) {
+//            for(int colIndex = 0; colIndex< brr[rowIndex].length-1; colIndex++) {
+//                System.out.print(brr[rowIndex][colIndex] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        // input for 2d array
+
+//        int crr[][] = new int [3][4];
+//        Scanner sc = new Scanner(System.in);
+//
+//        for(int i = 0; i <= crr.length-1; i++){
+//            for (int j = 0; j<=crr.length-1; j++) {
+//                System.out.print("Provide Value for row " + i + " Provide value for col " + j );
+//                crr[i][j] = sc.nextInt();
+//            }
+//        }
+//                for (int rowIndex = 0; rowIndex<= crr.length-1; rowIndex++) {
+//            for(int colIndex = 0; colIndex< crr[rowIndex].length-1; colIndex++) {
+//                System.out.print(crr[rowIndex][colIndex] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        // Sum in 2d array-
+//        int[][] drr = {{1,2,3}, {1,2,3}};
+//        int sum = 0;
+//
+//        for (int i = 0; i<drr.length; i++) {
+//            for (int j = 0; j<drr[i].length; j++) {
+//                int value = drr[i][j];
+//                sum = sum + value;
+//            }
+//        }
+//        System.out.println(sum);
+
+//        // Multiplication in 2d array-
+//        int[][] err = {{1,2,3}, {1,2,3}};
+//        int ans = 1;
+//
+//        for (int i = 0; i<err.length; i++) {
+//            for (int j = 0; j<err[i].length; j++) {
+//                int value = err[i][j];
+//                ans = ans * value;
+//            }
+//        }
+//        System.out.println(ans);
+
+        // max vlalue of a 2d array-
+        int frr[][] = {{2,3,5}, {34,43,21}};
+        int maxValue = frr[0][0];
+
+        for (int i = 0; i<frr.length; i++) {
+            for (int j = 0; j<frr[i].length; j++) {
+                if(frr[i][j] > maxValue) {
+                    // update max
+                    maxValue = frr[i][j];
+                }
+            }
+        }
+        System.out.println(maxValue);
+
+    }
+}
