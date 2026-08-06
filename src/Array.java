ba33e93b36cdde23afebc5e0b3ab8b97d5643647
Kushagra Bhatt <kushagrab453@gmail.com>
2026-08-06 21:58:31 +0530

    Add files via upload

---
+++ src/Array.java
@@ -0,0 +1,90 @@
+import java.util.*;
+public class Array {
+    public static void main(String[] args) {
+//        int arr[];
+//        arr = new int[5];
+//        int brr[] = {22,33,44,55,66};
+
+//        System.out.println("Value at index 1: " + brr[1]);
+//        System.out.println("Value at index 1: " + brr[0]);
+//        System.out.println("Value at index 1: " + brr[2]);
+//        System.out.println("Value at index 1: " + brr[3]);
+//        System.out.println("Value at index 1: " + brr[4]);
+
+        // Traversing using for loop-
+//        for (int index = 0; index<=brr.length-1; index++) {
+//            System.out.println(brr[index]);
+
+        // Using for each loop-
+//        for(int val: brr) {
+//            System.out.println(val);
+//        }
+
+        // Input in array
+//        int crr[] = new int[5];
+//        Scanner sc = new Scanner(System.in);
+//        int n = crr.length;
+//
+//        for(int i = 0; i<=n-1; i++) {
+//            crr[i] = sc.nextInt();
+//        }
+//  for (int val: crr) {
+//      System.out.println(val);
+//  }
+
+//  int drr[] = {24,124,134};
+//  int n = 3;
+//  for (int i = 0; i<=drr.length-1; i++) {
+//      System.out.println(drr[i]);
+//  }
+//
+
+//        // sum of array elements.
+//        int err[] = {10,20,30,40,50};
+//        int sum = 0;
+//        int n = err.length;
+//
+//        for(int i = 0; i<=n-1; i++) {
+//            int value = err[i];
+//            sum = sum + value;
+//        }
+//        System.out.println(sum);
+
+//        // Product of array elements.
+//        int frr[] = {2,4,6,8};
+//        int product = 1;
+//        int n = frr.length;
+//
+//        for (int i = 0; i<=n-1; i++) {
+//            int value = frr[i];
+//            product = product * value;
+//        }
+//        System.out.println(product);
+
+
+//          // find max element-
+//        int grr[] = {13,4,56,-7,8,33};
+//        int n = grr.length;
+//        int maxValue =grr[0];
+//
+//        for (int i =0; i <=n-1; i++) {
+//            if(grr[i] > maxValue) {
+//                maxValue = grr[i];
+//            }
+//        }
+//        System.out.println(maxValue);
+
+
+//        // find minimum value in array.
+//        int hrr[] = {13, 4, 56, -7, 8, 33};
+//        int n = hrr.length;
+//        int minValue = hrr[0];
+//
+//        for (int i = 0; i <= n - 1; i++) {
+//            if (hrr[i] < minValue) {
+//            }
+//            System.out.println(minValue);
+
+//        }
+    }
+}
