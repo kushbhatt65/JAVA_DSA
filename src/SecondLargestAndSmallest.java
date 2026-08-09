a0942b84928783a22452c4b7abd6ba0c017ed735
Kushagra Bhatt <kushagrab453@gmail.com>
2026-08-09 22:29:22 +0530

    Add files via upload

---
+++ src/SecondLargestAndSmallest.java
@@ -0,0 +1,34 @@
+public class SecondLargestAndSmallest {
+    public static void main(String [] args) {
+//        int[] arr = {12,43,56,635,653,74};
+//        int max = arr[0];
+//        for(int i = 0; i<arr.length; i++) {
+//            if (arr[i] > max) {
+//                max = arr[i];
+//            }
+//        }
+//        System.out.println(max);
+
+
+//        // find second largest.
+//        int[] arr = {12, 43, 56, 635, 653, 74};
+//        int max = Integer.MIN_VALUE;
+//        int secondMax = Integer.MIN_VALUE;
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] > max) {
+//                secondMax = max;   // shift old max down
+//                max = arr[i];      // update max
+//            } else if (arr[i] > secondMax && arr[i] != max) {
+//                secondMax = arr[i]; // update second max
+//            }
+//        }
+//
+//        System.out.println("Max: " + max);
+//        System.out.println("Second Max: " + secondMax);
+
+    }
+    }
+
+
+
